f86d30d10cfd2352f8ab9160ba0727174cf1db42
Billz2026 <billz_rida_37er@yahoo.co.uk>
2026-08-11 10:33:58 +0100

    Version premium render bridge

--- game/runtime-v23-main.js
+++ game/runtime-v23-main.js
@@ -4,7 +4,7 @@ import {
   renderHud, showResult, stageConfig, strikeQualityLabel, syncStage, MAX_LIVES, LIFE_STREAK_TARGET
 } from "./core-v6.js?v=7";
 import { resolveShotPhysics } from "./runtime-v23-bridge-physics-v19-f1d39f9409.js";
-import { resizeCanvas, drawScene } from "./runtime-v23-bridge-render-v17-3-1-64b7ab3399.js";
+import { resizeCanvas, drawScene } from "./runtime-v23-bridge-render-v17-3-1-64b7ab3399.js?v=30";
 import { unlockAudio, playImpactSound, playOutcomeSound, playStageSound } from "./audio-v10.js?v=10";
 import { difficultyForStage } from "./difficulty-v9.js?v=9";
 import { activeCharacter, meterMultiplier } from "./characters-v13.js?v=13";
