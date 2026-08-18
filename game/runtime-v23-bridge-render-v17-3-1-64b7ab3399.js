e46ede7de32f67b5726e919921f665c68b2e8985
Billz2026 <billz_rida_37er@yahoo.co.uk>
2026-08-18 13:33:44 +0100

    Use V51.1 arcade character module in runtime bridge

--- game/runtime-v23-bridge-render-v17-3-1-64b7ab3399.js
+++ game/runtime-v23-bridge-render-v17-3-1-64b7ab3399.js
@@ -1,5 +1,5 @@
 import { drawScene as drawBaseScene, resizeCanvas } from "./runtime-v23-generated-render-v17-v1731-7f257084b1.js";
-import { drawArcadeHeroCharacterV51 } from "./arcade-character-v51.js?v=51.0.0";
+import { drawArcadeHeroCharacterV51 } from "./arcade-character-v51.js?v=51.1.0";
 import { activeCharacter } from "./characters-v13.js?v=32.4";
 import { WORLD, state, ctx, canvasView } from "./core-v6.js?v=32.4";
 import { drawMatchdayImpact } from "./matchday-impact-v32.js?v=40.3.0";
