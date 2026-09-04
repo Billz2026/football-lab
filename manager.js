a4011b4004f0e9becd30a97451baeb3a17498559
Billz2026 <billz_rida_37er@yahoo.co.uk>
2026-09-04 20:53:46 +0100

    Load V0.4.7 pre-season layer

--- manager.js
+++ manager.js
@@ -1,3 +1,4 @@
 import './manager-base-v045.js?v=0.4.5';
 import './career-news-ui-v0461.js?v=0.4.6.1';
 import './career-news-ui-v0462-fix.js?v=0.4.6.2';
+import './career-preseason-ui-v047.js?v=0.4.7';
