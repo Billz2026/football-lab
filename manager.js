3995201789f582e17445442e259be3612576c501
Billz2026 <billz_rida_37er@yahoo.co.uk>
2026-09-04 20:55:58 +0100

    Load Pre-Season navigation compatibility fix

--- manager.js
+++ manager.js
@@ -2,3 +2,4 @@ import './manager-base-v045.js?v=0.4.5';
 import './career-news-ui-v0461.js?v=0.4.6.1';
 import './career-news-ui-v0462-fix.js?v=0.4.6.2';
 import './career-preseason-ui-v047.js?v=0.4.7';
+import './career-preseason-ui-v0471-fix.js?v=0.4.7.1';
