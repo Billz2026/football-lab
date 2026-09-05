ec8c1f5c2f7246ea11c51b159a0c847c076b0865
Billz2026 <billz_rida_37er@yahoo.co.uk>
2026-09-05 17:58:54 +0100

    Load Match Centre V3.3.2 density pass

--- manager.js
+++ manager.js
@@ -23,4 +23,5 @@ import './matchday-experience-v31.js?v=3.1.0';
 import './matchday-polish-v321.js?v=3.2.1';
 import './matchday-console-v33.js?v=3.3.0';
 import './matchday-console-v331-compat.js?v=3.3.1';
+import './matchday-density-v332.js?v=3.3.2';
 import './database-readonly-v1.js?v=1.0.0';
