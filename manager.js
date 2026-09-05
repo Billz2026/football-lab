ad163b2a937f05ba8d60dd55f20ad8304f4b5901
Billz2026 <billz_rida_37er@yahoo.co.uk>
2026-09-05 17:21:40 +0100

    Load Match Centre V3.3 compatibility layer

--- manager.js
+++ manager.js
@@ -22,4 +22,5 @@ import './matchday-xg-ui-v1.js?v=1.0.0';
 import './matchday-experience-v31.js?v=3.1.0';
 import './matchday-polish-v321.js?v=3.2.1';
 import './matchday-console-v33.js?v=3.3.0';
+import './matchday-console-v331-compat.js?v=3.3.1';
 import './database-readonly-v1.js?v=1.0.0';
