daa2838190f9a0dbe50a03a65f95559bef5ae0f2
Billz2026 <billz_rida_37er@yahoo.co.uk>
2026-09-05 17:40:23 +0100

    Load Match Centre V3.4.2 overlap fix

--- manager.js
+++ manager.js
@@ -24,5 +24,5 @@ import './matchday-polish-v321.js?v=3.2.1';
 import './matchday-console-v33.js?v=3.3.0';
 import './matchday-console-v331-compat.js?v=3.3.1';
 import './matchday-mode-v34.js?v=3.4.1';
-import './matchday-ratings-v341.js?v=3.4.1';
+import './matchday-ratings-v341.js?v=3.4.2';
 import './database-readonly-v1.js?v=1.0.0';
