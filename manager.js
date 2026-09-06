f0e90bbeb41dc856bd811d1e5b099679ce5c990a
Billz2026 <billz_rida_37er@yahoo.co.uk>
2026-09-06 20:33:04 +0100

    Load Commentary Engine V1 on matchday

--- manager.js
+++ manager.js
@@ -43,3 +43,4 @@ import './career-integrity-v067.js?v=0.6.7';
 import './match-fulltime-single-continue-v064.js?v=0.6.7';
 import './recording-fixes-v068.js?v=0.6.8';
 import './matchday-runtime-fixes-v070.js?v=0.7.0';
+import './commentary-engine-v1.js?v=1.0.0';
