a3fa152b5265bcda0cf817b2bc86f25cce52dd1f
Billz2026 <billz_rida_37er@yahoo.co.uk>
2026-09-10 10:19:09 +0100

    Warm player profiles before first click

--- manager.js
+++ manager.js
@@ -7,4 +7,5 @@
  */
 
 import './manager-runtime-v1.js?v=1.1.0';
-import './player-profile-fast-v1.js?v=1.0.0';
+import './profile-preload-v1.js?v=1.0.0';
+import './player-profile-fast-v1.js?v=1.0.1';
