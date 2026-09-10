9a3e0712154fd3a17f7e96029aefc69dcaf4da70
Billz2026 <billz_rida_37er@yahoo.co.uk>
2026-09-10 19:30:06 +0100

    Bump Fold homepage stylesheet cache version

--- mobile-home-v049.js
+++ mobile-home-v049.js
@@ -3,7 +3,7 @@ if (!document.getElementById(id)) {
   const link = document.createElement('link');
   link.id = id;
   link.rel = 'stylesheet';
-  link.href = './mobile-home-v049.css?v=0.4.10';
+  link.href = './mobile-home-v049.css?v=0.4.11';
   document.head.appendChild(link);
 }
 
