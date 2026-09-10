d9dd759505ab823e2ffe2f203aa3ae90c9004f70
Billz2026 <billz_rida_37er@yahoo.co.uk>
2026-09-10 22:07:03 +0100

    Load corrected unfolded Fold manager layout

--- manager.js
+++ manager.js
@@ -24,4 +24,4 @@ import './transfer-market-filters-v2.js?v=2.1.0';
 import './transfer-interest-ui-v1.js?v=1.0.1';
 import './manager-create-responsive-v1.js?v=1.0.0';
 import './manager-create-responsive-v2.js?v=2.0.0';
-import './manager-create-responsive-v3.js?v=3.0.0';
+import './manager-create-responsive-v3.js?v=3.1.0';
