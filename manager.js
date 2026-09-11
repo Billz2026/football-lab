0786fdbdee1a402a26ea7aa611280131bce9a8fc
Billz2026 <billz_rida_37er@yahoo.co.uk>
2026-09-11 16:01:55 +0100

    Load transfer player navigation improvements

--- manager.js
+++ manager.js
@@ -23,6 +23,7 @@ import './career-ui-polish-v2.js?v=2.0.1';
 import './player-profile-cm-v2.js?v=2.0.0';
 import './player-performance-v1.js?v=1.0.0';
 import './transfer-market-filters-v2.js?v=2.1.0';
+import './transfer-player-navigation-v1.js?v=1.0.0';
 import './transfer-interest-ui-v1.js?v=1.0.1';
 import './manager-create-responsive-v1.js?v=1.0.0';
 import './manager-create-responsive-v2.js?v=2.0.0';
